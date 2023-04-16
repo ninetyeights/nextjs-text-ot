223b14eb13db7b58e27ee98a235bd7347fa03ea5
Rain <clover1998love@gmail.com>
2023-04-15 20:44:55 -0400

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <nav className='space-x-4'>
+    <Link href="/first">First</Link>
+    <Link href="/second">Second</Link>
+    <Link href="/third">Third</Link>
+  </nav>
+    <Component {...pageProps} />
+  </>
 }
